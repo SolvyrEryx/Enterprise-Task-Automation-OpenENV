43f9671603c20e63d401039ce0d0fc6a74079a9f
Solvyr Eryx <rahuljaiprakashden@gmail.com>
2026-04-11 13:25:06 +0530

    Add files via upload

---
+++ tasks/__init__.py
@@ -0,0 +1,7 @@
+"""Task specifications for Enterprise Task Automation Environment"""
+
+from .easy import TASK_CONFIG as EASY_CONFIG
+from .medium import TASK_CONFIG as MEDIUM_CONFIG
+from .hard import TASK_CONFIG as HARD_CONFIG
+
+__all__ = ["EASY_CONFIG", "MEDIUM_CONFIG", "HARD_CONFIG"]
